165e82392c2a4272c710f18908a2cd0c62261738
Luke <lechambers@gmail.com>
2022-09-15 12:41:42 -0400

    Created monster.py Updated pawn.py with some more member variables

---
+++ entities/monster.py
@@ -0,0 +1,6 @@
+from entities.pawn import Pawn
+
+
+class Monster(Pawn):
+    def __init__(self, name:str, position:tuple[int]=(0,0)):
+        super().__init__(name, position)
--- entities/pawn.py
+++ entities/pawn.py
@@ -2,10 +2,22 @@ from entities.effects import Effect
 from entities.stats import Stats
 
 
+#TODO: consider making this an abstract base class
 class Pawn:
+    name: str
+    position: tuple[int]
     effects: list[Effect]
     stats: Stats
+
+    #TODO: needs to be filled out
+    def __init__(self, name:str, position:tuple[int]) -> None:
+        self.name = name
+        self.position = position
 
     @property
     def has_effects(self) -> bool:
         return len(self.effects) > 0
+
+    def move(self, x:int, y:int) -> None:
+        pass
+

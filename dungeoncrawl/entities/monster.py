e00d85b8f7ddbc25aeb631cb3af8fc64de31afcc
Luke <lechambers@gmail.com>
2022-09-18 16:23:45 -0400

    added type ignore for sonarlint

--- dungeoncrawl/entities/monster.py
+++ dungeoncrawl/entities/monster.py
@@ -2,5 +2,5 @@ from dungeoncrawl.entities.pawn import Pawn
 
 
 class Monster(Pawn):
-    def __init__(self, name: str, position: tuple[int] = (0, 0)):
+    def __init__(self, name: str, position: tuple[int] = (0, 0)): # type: ignore
         super().__init__(name, position)

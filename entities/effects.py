20e4e8e520d65cedb5c07a7cfe9451e7bcbe42f3
Luke <lechambers@gmail.com>
2022-09-17 20:09:22 -0400

    added fire effect; edited docstring.
    I'm considering consolidating everything to "damage" instead of
    "change_damage_dealt", so it's just simpler to add one thing to
    pawn damage each time -- just whatever result plus effect.damage

--- entities/effects.py
+++ entities/effects.py
@@ -12,7 +12,7 @@ class Effect:
 
     `remaining_duration` [int|float("inf")] is the duration (in turns) of the effect. Duration will be counted down by the Board. The default duration is infinite (`float("inf")`).  
 
-    `change_damage_dealt` [int] is an amount of bonus damage that the Pawn this effect is applied to can deal while it has this effect. Positive values add bonus damage to a Pawn's attack, negative values decrease the Pawn's damage. The default value is zero.  
+    `change_damage_dealt` [int] is supposed to be an amount of bonus damage that the Pawn this effect is applied to can deal while it has this effect. Positive values add bonus damage to a Pawn's attack, negative values decrease the Pawn's damage. The default value is zero.  
 
     `change_movement` [int] is an integer amount of bonus grid squares that the pawn will be able to move each turn. See the Haste effect for an example of this. Positive values will increase the number of squares that a Pawn can move, and negative values will decrease the number of squares. The default value is zero.  
 
@@ -41,3 +41,5 @@ Bless = Effect(name="Bless", damage=0, remaining_duration=3,
 # Equipment effects
 Curse = Effect(name="Curse", damage=0, remaining_duration=float(
     'inf'), change_damage_dealt=-1, change_movement=0)
+Fire = Effect(name="Fire", damage=2, remaining_duration=0,
+              change_damage_dealt=0, change_movement=0)

5642f1080fbe0e5a382786de8a9a2819e1577bab
Luke <lechambers@gmail.com>
2022-12-19 22:13:41 -0500

    Update pawn.py
    * Changed Point to a class rather than a namedtuple to ensure that the x,y variables were mutable.
    * added measurement related methods to Pawn
    * added some quick tests in __main__

--- dungeoncrawl/entities/pawn.py
+++ dungeoncrawl/entities/pawn.py
@@ -1,10 +1,14 @@
-from collections import namedtuple
-from typing import NamedTuple
+from dataclasses import dataclass
+from functools import singledispatchmethod
 
-from dungeoncrawl.entities.effects import Effects
-from dungeoncrawl.entities.stats import Stats
+from effects import Effects
+from stats import Stats
 
-Point = namedtuple('Point', ['x', 'y'])
+
+@dataclass
+class Point:
+    x: int
+    y: int
 
 
 class Pawn:
@@ -28,22 +32,92 @@ class Pawn:
     @property
     def has_effects(self) -> bool:
         return self.effects.active
+    
+    #######################
+    # ~~~ Measurements ~~~#
+    #######################
+    @singledispatchmethod
+    def distance_to(self, other: 'Pawn') -> float:
+        return ((self.position.x - other.position.x) ** 2 + (self.position.y - other.position.y) ** 2) ** 0.5
+    
+    @distance_to.register
+    def _(self, other: Point) -> float:
+        return ((self.position.x - other.x) ** 2 + (self.position.y - other.y) ** 2) ** 0.5
 
+    @distance_to.register
+    def _(self, x:int, y:int) -> float:
+        return ((self.position.x - x) ** 2 + (self.position.y - y) ** 2) ** 0.5
+
+    @singledispatchmethod
+    def distance_from(self, other: 'Pawn') -> float:
+        return self.distance_to(other)
+
+    @distance_from.register
+    def _(self, other: Point) -> float:
+        return self.distance_to(other)
+
+    @distance_from.register
+    def _(self, x:int, y:int) -> float:
+        return self.distance_to(x, y)
+    
+    
+    ###################
+    # ~~~ Movement ~~~#
+    ###################
+    @singledispatchmethod
+    def move_to(self, x: int, y: int) -> None:
+        self.position = Point(x, y)
+
+    @move_to.register
+    def _(self, point: Point) -> None:
+        self.position = point
+
+    def move_right(self) -> None:
+        self.position.x += 1
+
+    def move_left(self) -> None:
+        self.position.x -= 1
+
+    def move_up(self) -> None:
+        self.position.y += 1
+
+    def move_down(self) -> None:
+        self.position.y -= 1
+
+    #################
+    # ~~~ Status ~~~#
+    #################
     @property
     def alive(self) -> bool:
         return self.health > 0
 
-    def move_to(self, x: int, y: int) -> None:
-        self.position = Point(x, y)
 
-    def move_left(self) -> None:
-        self.position = Point(self.position.x-1, self.position.y)
+if __name__ == '__main__':
+    import random
 
-    def move_right(self) -> None:
-        self.position = Point(self.position.x+1, self.position.y)
+    a = Pawn('a', Point(random.randint(0,10), random.randint(0,10)), 100)
+    b = Pawn('b', Point(random.randint(0,10), random.randint(0,10)), 100)
+    # testing creation
+    print("\nPawns:")
+    print(f"a: {a.name} at {a.position}")
+    print(f"b: {b.name} at {b.position}")
 
-    def move_up(self) -> None:
-        self.position = Point(self.position.x, self.position.y+1)
+    # testing measurements
+    print("\nCalculations:")
+    print(f"distance from a -> b: ", a.distance_to(b))
+    print(f"distance from a -> (2, 2): ", a.distance_to(2, 2))
 
-    def move_down(self) -> None:
-        self.position = Point(self.position.x, self.position.y-1)
+    # testing movement
+    print("\nMovement:")
+    print(f"moving a to (2, 2)")
+    a.move_to(Point(2, 2))
+    print(f"a: {a.name} at {a.position}")
+    print(f"moving a to (3, 3)")
+    a.move_to(3, 3)
+    print(f"moving down")
+    a.move_down()
+    print(f"a: {a.name} at {a.position}")
+    print("moving left")
+    a.move_left()
+    print(f"a: {a.name} at {a.position}")
+
